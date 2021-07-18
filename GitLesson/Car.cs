825fea23e912dd90753a354e4f4c39a0c81d12df
Ilie <soloduh.ilie@gmail.com>
2021-07-18 14:25:50 +0300

    Added Car class

---
+++ GitLesson/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitLesson
+{
+    class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("Hello from Car class!");
+        }
+    }
+}
