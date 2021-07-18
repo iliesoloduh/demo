55fb3081b33127f8fa9957672ff492240a8209ee
Ilie <soloduh.ilie@gmail.com>
2021-07-18 13:47:40 +0300

    Initial commit

---
+++ GitLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
